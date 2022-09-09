b7e5e71933eeac1d1bb955fb1fcbfd13c1c0fa5e
Merzouk MENHOUR <merzouk@gmail.com>
2022-09-09 11:24:23 +0200

    modifs

--- saisie_manuelle.c
+++ saisie_manuelle.c
@@ -13,40 +13,6 @@
 
 #include "header.h"
 
-/*
-void viderBuffer()
-{
-    int c = 0;
-    while (c != '\n' && c != EOF)
-    {
-        c = getchar();
-    }
-}
-
-int lire(char *chaine, int longueur)
-{
-    char *positionEntree = NULL;
-
-    if (fgets(chaine, longueur, stdin) != NULL)
-    {
-        positionEntree = strchr(chaine, '\n');
-        if (positionEntree != NULL)
-        {
-            *positionEntree = '\0';
-        }
-        else
-        {
-            viderBuffer();
-        }
-        return 1;
-    }
-    else
-    {
-        viderBuffer();
-        return 0;
-    }
-}
-*/
 
 int lire(char *chaine, int longueur)
 {
