78d4865d320656459291f7a01932809e87337236
Merzouk MENHOUR <merzouk@gmail.com>
2024-02-15 07:15:44 +0100

    update sources

--- sources/demo.c
+++ sources/demo.c
@@ -45,11 +45,22 @@ void demo(char *min, char *maj)
 
 	char * source2 = "Hello tout le monde depuis paris!";
 	char * cypher_key2 = "cryptographie";
-	printf("\n\nCle de chiffremment = %s \ntexte clair = %s\n\n", cypher_key1, source1);
+	printf("\n\nCle de chiffremment = %s \ntexte clair = %s\n\n", cypher_key2, source2);
 	char * cypher2 = encrypt_source(source2, cypher_key2, min, maj);
 	printf("\nLe chiffre =  %s\n", cypher2);
 	char * clair2 = decrypt_cypher(cypher_key2, cypher2, min, maj);
 	printf("\nLe dichiffre = %s  \n", clair2);
+
+
+	printf("\n=================== Exemple 4 ========================\n");
+
+	char * source3 = "Hello tout le monde depuis paris!";
+	char * cypher_key3 = "haropiro";
+	printf("\n\nCle de chiffremment = %s \ntexte clair = %s\n\n", cypher_key3, source3);
+	char * cypher3 = encrypt_source(source3, cypher_key3, min, maj);
+	printf("\nLe chiffre =  %s\n", cypher3);
+	char * clair3 = decrypt_cypher(cypher_key3, cypher3, min, maj);
+	printf("\nLe dichiffre = %s  \n", clair3);
 
 	printf("\n=================== Fin demo ========================\n");
 }
